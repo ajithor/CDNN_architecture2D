41ac5bd0ef925f8d04990c24dc59f38cbd4835c7
ajithor <ramnath216nayak7@gmail.com>
2018-04-04 02:35:13 +0530

    Presentation changes from previous commit

--- conv_imgsize.py
+++ conv_imgsize.py
@@ -1,16 +1,14 @@
 x = int(input("Enter the pixel value\n"))
 y = int(input())
 print("\nFor Convolving")
-csx = int(input("Enter the stride_x"))
-csy = int(input("Enter the Stride_y"))
-cfx = int(input("Ener Fliter_x"))
-cfy = int(input("Ener Fliter_y"))
-cfno = int(input("Enter no. of filters"))
-#cpa = input("Zero Padding? (S/V)")
-zpx = int(input("Zero padding_X"))
-zpy = int(input("Zero padding_Y"))
-l = int(input("Enter no. of layers "))
+csx = int(input("Stride_x : "))
+csy = int(input("Stride_y : "))
+cfx = int(input("Fliter_x : "))
+cfy = int(input("Fliter_y : "))
+zpx = int(input("Zero padding_x : "))
+zpy = int(input("Zero padding_y : "))
 
+l = int(input("Enter no. of layers : "))
 
 print("\nFor pooling")
 psx = int(input("Enter the stride_x"))
@@ -18,13 +16,15 @@ psy = int(input("Enter the Stride_y"))
 pfx = int(input("Ener Fliter_x"))
 pfy = int(input("Ener Fliter_y"))
 
+print("INPUT -->[", str(x), " X ", str(y), "]")
+
 for i in range(1,l+1):
 	#CONV
 	
 	x = (x - cfx + (2 * zpx)) / csx +1		
 	y = (y - cfy + (2 * zpy)) / csy +1
 
-	print("CONV--> [", str(int(x)), " X ",str(int(y)), end = "]\n")
+	print("CONV--> [", str(int(x)), " X ",str(int(y)), end = "] ")
 	#pool
 
 	x = (x - pfx) / psx + 1
